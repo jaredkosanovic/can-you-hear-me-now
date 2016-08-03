fccb06bb9284978ab0ff53219ba281a0fc9eff1e
jaredkosanovic <jared.tennismaster@gmail.com>
2016-08-02 20:47:08 -0700

    Add files via upload

---
+++ Can you hear me now/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Can_you_hear_me_now
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trans1x = new System.Windows.Forms.TextBox();
+            this.trans1y = new System.Windows.Forms.TextBox();
+            this.trans2y = new System.Windows.Forms.TextBox();
+            this.trans2x = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.wavelengthtext = new System.Windows.Forms.TextBox();
+            this.draw = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(260, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Can You Hear Me Now?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Signal Strength Graph";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Graph Area";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Maximum X: 400";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Maximum Y: 400";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(71, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Transmitter 1";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "X-coordinate";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(148, 218);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Y-coordinate";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trans1x
+            // 
+            this.trans1x.Location = new System.Drawing.Point(36, 234);
+            this.trans1x.MaxLength = 3;
+            this.trans1x.Name = "trans1x";
+            this.trans1x.Size = new System.Drawing.Size(64, 20);
+            this.trans1x.TabIndex = 9;
+            // 
+            // trans1y
+            // 
+            this.trans1y.Location = new System.Drawing.Point(151, 234);
+            this.trans1y.MaxLength = 3;
+            this.trans1y.Name = "trans1y";
+            this.trans1y.Size = new System.Drawing.Size(64, 20);
+            this.trans1y.TabIndex = 10;
+            // 
+            // trans2y
+            // 
+            this.trans2y.Location = new System.Drawing.Point(151, 315);
+            this.trans2y.MaxLength = 3;
+            this.trans2y.Name = "trans2y";
+            this.trans2y.Size = new System.Drawing.Size(64, 20);
+            this.trans2y.TabIndex = 15;
+            // 
+            // trans2x
+            // 
+            this.trans2x.Location = new System.Drawing.Point(36, 315);
+            this.trans2x.MaxLength = 3;
+            this.trans2x.Name = "trans2x";
+            this.trans2x.Size = new System.Drawing.Size(64, 20);
+            this.trans2x.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(148, 299);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Y-coordinate";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(33, 299);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "X-coordinate";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Blue;
+            this.label11.Location = new System.Drawing.Point(71, 272);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 15);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Transmitter 2";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(33, 369);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Wavelength";
+            // 
+            // wavelengthtext
+            // 
+            this.wavelengthtext.Location = new System.Drawing.Point(132, 369);
+            this.wavelengthtext.MaxLength = 8;
+            this.wavelengthtext.Name = "wavelengthtext";
+            this.wavelengthtext.Size = new System.Drawing.Size(83, 20);
+            this.wavelengthtext.TabIndex = 17;
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(36, 436);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 23);
+            this.draw.TabIndex = 18;
+            this.draw.Text = "Draw";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(140, 436);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 19;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 484);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.wavelengthtext);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.trans2y);
+            this.Controls.Add(this.trans2x);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.trans1y);
+            this.Controls.Add(this.trans1x);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Can You Hear Me Now?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox trans1x;
+        private System.Windows.Forms.TextBox trans1y;
+        private System.Windows.Forms.TextBox trans2y;
+        private System.Windows.Forms.TextBox trans2x;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox wavelengthtext;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ Can you hear me now/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Can_you_hear_me_now
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            //declaring local variables for input validation
+            int tran1x = 0;
+            int tran1y = 0;
+            int tran2x = 0;
+            int tran2y = 0;
+
+            double wavelength = 0;
+            
+            //input validation for transmitter 1 x coordinate
+            do
+            {
+                try
+                {
+                    tran1x = System.Convert.ToInt16(trans1x.Text);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Not a valid X coordinate for transmitter 1!");
+                    return;
+                }
+
+                if ((tran1x < 1) || (tran1x > 400))
+                {
+                    MessageBox.Show("Not a valid X coordinate for transmitter 1!");
+                    return;        
+                }
+
+            }
+            while ((tran1x < 1) || (tran1x > 400));
+
+            //input validation for transmitter 1 y coordinate
+            do
+            {
+                try
+                {
+                    tran1y = System.Convert.ToInt16(trans1y.Text);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Not a valid Y coordinate for transmitter 1!");
+                    return;
+                }
+
+                if ((tran1y < 1) || (tran1y > 400))
+                {
+                    MessageBox.Show("Not a valid Y coordinate for transmitter 1!");
+                    return;
+                }
+
+            }
+            while ((tran1y < 1) || (tran1y > 400));
+
+            //input validation for transmitter 2 x coordinate
+            do
+            {
+                try
+                {
+                    tran2x = System.Convert.ToInt16(trans2x.Text);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Not a valid X coordinate for transmitter 2!");
+                    return;
+                }
+
+                if ((tran2x < 1) || (tran2x > 400))
+                {
+                    MessageBox.Show("Not a valid X coordinate for transmitter 2!");
+                    return;
+                }
+
+            }
+            while ((tran2x < 1) || (tran2x > 400));
+
+            //input validation for transmitter 2 y coordinate
+            do
+            {
+                try
+                {
+                    tran2y = System.Convert.ToInt16(trans2y.Text);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Not a valid Y coordinate for transmitter 2!");
+                    return;
+                }
+
+                if ((tran2y < 1) || (tran2y > 400))
+                {
+                    MessageBox.Show("Not a valid Y coordinate for transmitter 2!");
+                    return;
+                }
+
+            }
+            while ((tran2y < 1) || (tran2y > 400));
+
+            //input validation for wavelength value
+            do
+            {
+                try
+                {
+                    wavelength = System.Convert.ToDouble(wavelengthtext.Text);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Not a valid wavelength value! Must be greater than 0.");
+                    return;
+                }
+
+                if (wavelength <= 0)
+                {
+                    MessageBox.Show("Not a valid wavelength value! Must be greater than 0.");
+                    return;
+                }
+            }
+            while (wavelength <= 0);
+
+            //declaring the transmitters
+            transmitter Trans1 = new transmitter();
+            transmitter Trans2 = new transmitter();
+
+            //and the antenna
+            antenna MyPhone = new antenna();
+
+            //settings the x and y coordinates of the transmitters in their classes
+            Trans1.Set_X(tran1x);
+            Trans1.Set_Y(tran1y);
+
+            Trans2.Set_X(tran2x);
+            Trans2.Set_Y(tran2y);
+            
+            //new drawing pen
+            System.Drawing.Pen RectanglePen = new System.Drawing.Pen(Color.Black, 1);
+
+            int PBwidth = pictureBox1.Width;
+            int PBheight = pictureBox1.Height;
+
+            for (int Xpos = 0; Xpos < PBwidth; Xpos++) //Loops through all X coordinates
+            {
+                for (int Ypos = 0; Ypos < PBheight; Ypos++) //Loops through all Y coordinates at each X
+                {
+                    MyPhone.Set_X(Xpos);
+                    MyPhone.Set_Y(Ypos);
+
+                    //Get the signal strength from the antenna object
+                    double SignalStrength = MyPhone.signal_strength(Trans1.Get_X(), Trans1.Get_Y(), Trans2.Get_X(), Trans2.Get_Y(), wavelength);
+
+                    //Convert the signal strength into an RGB color with 100% strength equal to RGB 255,255,255
+                    int ColorStrength = System.Convert.ToInt16(255.0 * (SignalStrength / 2.0));
+
+                    //Set the pen color based on the signal strength
+                    RectanglePen.Color = System.Drawing.Color.FromArgb(ColorStrength, ColorStrength, ColorStrength);
+
+                    //Draws the rectangle at the given coordinates
+                    pictureBox1.CreateGraphics().DrawRectangle(RectanglePen, Xpos, PBheight - Ypos, 1, 1);
+                }
+            }
+            
+            //draws a red square around where transmitter 1 is located
+            for (int Xpos = (Trans1.Get_X() - 4); Xpos <= (Trans1.Get_X() + 4); Xpos++)
+            {
+                for (int Ypos = (Trans1.Get_Y() - 4); Ypos <= (Trans1.Get_Y() + 4); Ypos++)
+                {
+                    RectanglePen.Color = System.Drawing.Color.FromName("Red");
+
+                    pictureBox1.CreateGraphics().DrawRectangle(RectanglePen, Xpos, (400 - Ypos), 1, 1);
+                }
+            }
+
+            //draws a blue square around where transmitter 2 is located
+            for (int Xpos = (Trans2.Get_X() - 4); Xpos <= (Trans2.Get_X() + 4); Xpos++)
+            {
+                for (int Ypos = (Trans2.Get_Y() - 4); Ypos <= (Trans2.Get_Y() + 4); Ypos++)
+                {
+                    RectanglePen.Color = System.Drawing.Color.FromName("Blue");
+
+                    pictureBox1.CreateGraphics().DrawRectangle(RectanglePen, Xpos, (400 - Ypos), 1, 1);
+                }
+            }
+        }
+
+        //the exit button closes the program
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    //simple transmitter class...stores x and y values
+    class transmitter
+    {
+        private int x_coordinate;
+        private int y_coordinate;
+
+        public int Get_X() { return(x_coordinate); }
+        public int Get_Y() { return(y_coordinate); }
+        public void Set_X(int X) { x_coordinate = X; }
+        public void Set_Y(int Y) { y_coordinate = Y; }
+    }
+    //antenna class, similar to transmitter except it can calculate signal strength
+    class antenna
+    {
+        private int ant_x_coord;
+        private int ant_y_coord;
+
+        public int Get_X() { return (ant_x_coord); }
+        public int Get_Y() { return (ant_y_coord); }
+        public void Set_X(int X) { ant_x_coord = X; }
+        public void Set_Y(int Y) { ant_y_coord = Y; }
+ 
+        public double signal_strength(int x1, int y1, int x2, int y2, double wave)
+        {
+            double pi = Math.PI;
+            double d1, d2, sig_strength;
+
+            //calculating the distance between the antenna and the transmitter using the Pythagorean theorm
+            d1 = (Math.Sqrt((Math.Pow((ant_x_coord - x1), 2)) + (Math.Pow((ant_y_coord - y1), 2))));
+            d2 = (Math.Sqrt((Math.Pow((ant_x_coord - x2), 2)) + (Math.Pow((ant_y_coord - y2), 2))));
+
+            //calcuting the signal strength using the supplied formula
+            sig_strength = (1 + Math.Sin((pi / 2) + (Math.Abs(d1 - d2) / wave * (2 * pi))));
+
+            return sig_strength;
+        }
+    } 
+} //scroll up, why are you down here?
